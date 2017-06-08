7ab7595a91b3ef0ebdfa5add7568904da163f262
Wwawawa <421299099@qq.com>
2017-06-08 17:25:21 +0800

    Create ViewClaim.cs

---
+++ Windows-Authentication/Models/ViewClaim.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthenticationDemo.Models
+{
+    public class ViewClaim
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+    }
+}
