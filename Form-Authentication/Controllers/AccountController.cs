05ec6cabaab49f3c7f8da94f55ddb8245f153f96
Wwawawa <421299099@qq.com>
2017-06-09 17:41:04 +0800

    Create AccountController.cs

---
+++ Form-Authentication/Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+using AuthenticationDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace AuthenticationDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // POST: /Account/Login
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model, string returnUrl)
+        {
+            // check if all required fields are set
+            if (ModelState.IsValid)
+            {
+                // authenticate user
+                var success = ValidateUser(model.UserName, model.Password);
+                //var success = Membership.ValidateUser(model.UserName, model.Password);
+
+                if (success)
+                {
+                    // set authentication cookie
+                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+
+                    return RedirectToLocal(returnUrl);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            ModelState.AddModelError("", "The user name or password provided is incorrect.");
+            return View(model);
+        }
+
+        private bool ValidateUser(string userName, string password)
+        {
+            return (userName == password);
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
