257f5defe21a2f01608453b1acd46a9805d41e70
Wwawawa <421299099@qq.com>
2017-06-15 18:26:59 +0800

    Create ClaimsTransformer.cs

---
+++ Authentication-Sessions/Security/ClaimsTransformer.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Services;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Security;
+
+namespace Web.Security
+{
+    public class ClaimsTransformer : ClaimsAuthenticationManager
+    {
+        public override ClaimsPrincipal Authenticate(string resourceName, ClaimsPrincipal incomingPrincipal)
+        {
+            if (!incomingPrincipal.Identity.IsAuthenticated)
+            {
+                return base.Authenticate(resourceName, incomingPrincipal);
+            }
+
+            var principal= CreatePrincipal(incomingPrincipal.Identity.Name);
+
+            EstablishSession(principal);
+
+            return principal;
+        }
+        private void EstablishSession(ClaimsPrincipal principal)
+        {
+            var sessionToken = new SessionSecurityToken(principal, TimeSpan.FromHours(8));
+            FederatedAuthentication.SessionAuthenticationModule.WriteSessionTokenToCookie(sessionToken);
+        }
+        private ClaimsPrincipal CreatePrincipal(string userName)
+        {
+            var claims = new List<Claim>();
+            if (userName == "dom")
+            {
+                claims.Add(new Claim(ClaimTypes.Name, userName));
+                claims.Add(new Claim(ClaimTypes.GivenName, "Dominick"));
+
+                Roles.GetRolesForUser(userName).ToList().ForEach(role => claims.Add(new Claim(ClaimTypes.Role, role)));
+            }
+            else
+            {
+                claims.Add(new Claim(ClaimTypes.Name, userName));
+                claims.Add(new Claim(ClaimTypes.GivenName, userName));
+            }
+            return new ClaimsPrincipal(new ClaimsIdentity(claims, "Custom"));
+        }
+    }
+}
