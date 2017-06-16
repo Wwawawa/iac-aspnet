de9a964eacc2d92c74135e61bbe66e425433915d
Wwawawa <421299099@qq.com>
2017-06-16 18:52:37 +0800

    Update CustomersController.cs

--- 5-Authorizaiton/Controllers/Api/CustomersController.cs
+++ 5-Authorizaiton/Controllers/Api/CustomersController.cs
@@ -5,11 +5,12 @@ namespace ClaimsBasedAuthorization.Controllers.Api
 {
     public class CustomersController : ApiController
     {
+        // http://localhost:1096/api/customers/
         public string Get()
         {
             return "OK";
         }
-
+        // http://localhost:1096/api/customers/1
         public string Get(int id)
         {
             var allowed = ClaimsAuthorization.CheckAccess("Get", "Customer", id.ToString());
