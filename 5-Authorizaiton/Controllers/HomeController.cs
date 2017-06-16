91a55b2bc93aa03055370d603e2eec3db8db00b0
Wwawawa <421299099@qq.com>
2017-06-16 18:41:04 +0800

    Create HomeController.cs

---
+++ 5-Authorizaiton/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System.IdentityModel.Services;
+using System.Security.Permissions;
+using System.Web.Mvc;
+using Thinktecture.IdentityModel.Authorization.Mvc;
+
+namespace ClaimsBasedAuthorization.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        [ClaimsAuthorize("View", "About")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        [ClaimsAuthorize("View", "StreetAddress", "TelephoneNumber")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
